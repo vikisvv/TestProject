627e92be704a11a715b4edefdd265e53caf6ef1b
optofizik <optofizik@gmail.com>
2015-05-28 20:08:57 +0300

    New2

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.MyMethod();
+        }
+    }
+
+    public static class MyClass
+    {
+        public static void MyMethod()
+        {
+            Console.WriteLine("Jakimov");
+        }
+    }
+}
